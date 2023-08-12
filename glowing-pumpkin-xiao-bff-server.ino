fa5541a8fd1414dcc8b9665615641c6920849fcc
John M. Wargo <johnwargo@users.noreply.github.com>
2023-08-12 16:19:52 -0400

    got the threading stuff working
    adding web server.
    Won't currently connect to Wi-Fi

--- glowing-pumpkin-xiao-bff-server.ino
+++ glowing-pumpkin-xiao-bff-server.ino
@@ -13,13 +13,34 @@
 // https://randomnerdtutorials.com/esp32-dual-core-arduino-ide/
 
 #include <FastLED.h>
+#include <WiFi.h>
+
+// Local constants
+#include "constants.h"
 
 #define DEBUG true
 #define NUM_LEDS 25
 #define PIN A3
 
+// Replace with your network credentials
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
 TaskHandle_t Task1;
 TaskHandle_t Task2;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0;
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
 int numColors = 6;
 uint32_t colors[] = { CRGB::Blue, CRGB::Green, CRGB::Orange, CRGB::Purple, CRGB::Red, CRGB::Yellow };
@@ -28,6 +49,7 @@ CRGB leds[NUM_LEDS];
 
 void setup() {
   Serial.begin(115200);
+  delay(500);
 
   // Initialize the FastLED library
   FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
@@ -35,8 +57,20 @@ void setup() {
   // Flash the lights RED twice
   flashLEDs(CRGB::Red, 2, 250);
 
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
 
   //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
   xTaskCreatePinnedToCore(
@@ -86,10 +120,52 @@ void Task2code(void* pvParameters) {
   Serial.println(xPortGetCoreID());
 
   for (;;) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(700);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(700);
+    WiFiClient client = server.available();  // Listen for incoming clients
+
+    if (client) {  // If a new client connects,
+      Serial.println("Client connection");
+      String currentLine = "";                                                   // make a String to hold incoming data from the client
+      while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+        currentTime = millis();
+        if (client.available()) {  // if there's bytes to read from the client,
+          char c = client.read();  // read a byte, then
+          Serial.write(c);         // print it out the serial monitor
+          header += c;
+          if (c == '\n') {  // if the byte is a newline character
+            // if the current line is blank, you got two newline characters in a row.
+            // that's the end of the client HTTP request, so send a response:
+            if (currentLine.length() == 0) {
+              // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+              // and a content-type so the client knows what's coming, then a blank line:
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println("Connection: close");
+              client.println();
+
+              // Display the HTML web page
+              client.println("<!DOCTYPE html><html>");
+              client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>");
+              client.println("<body><h1>ESP32 Web Server</h1>");
+              client.println("</body></html>");
+              // The HTTP response ends with another blank line
+              client.println();
+              // Break out of the while loop
+              break;
+            } else {  // if you got a newline, then clear currentLine
+              currentLine = "";
+            }
+          } else if (c != '\r') {  // if you got anything else but a carriage return character,
+            currentLine += c;      // add it to the end of the currentLine
+          }
+        }
+      }
+      // Clear the header variable
+      header = "";
+      // Close the connection
+      client.stop();
+      Serial.println("Client disconnected.");
+      Serial.println("");
+    }
   }
 }
 
