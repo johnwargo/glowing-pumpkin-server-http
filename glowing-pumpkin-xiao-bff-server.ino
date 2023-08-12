e9a3747a9493714cb48e705816b219d5d83f8717
John M. Wargo <johnwargo@users.noreply.github.com>
2023-08-12 15:33:59 -0400

    Update glowing-pumpkin-xiao-bff-server.ino

--- glowing-pumpkin-xiao-bff-server.ino
+++ glowing-pumpkin-xiao-bff-server.ino
@@ -2,11 +2,15 @@
 * Glowing Pumpkin 
 * 
 * Xaio ESP32 S3 version using the Adafruit 5x5 BFF LED
-* Matrix.
+* Matrix.  This edition adds a web server and web app
+* to the project so you can control it from a smartphone
+* or other external device.
 *
 * By John M. Wargo
 * https://johnwargo.com
 **********************************************************/
+
+// https://randomnerdtutorials.com/esp32-dual-core-arduino-ide/
 
 #include <FastLED.h>
 
@@ -14,19 +18,55 @@
 #define NUM_LEDS 25
 #define PIN A3
 
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
 int numColors = 6;
 uint32_t colors[] = { CRGB::Blue, CRGB::Green, CRGB::Orange, CRGB::Purple, CRGB::Red, CRGB::Yellow };
-CRGB leds[NUM_LEDS];  // LED Array (internal memory structure from FastLED)
+// LED Array (internal memory structure from FastLED)
+CRGB leds[NUM_LEDS];
 
 void setup() {
+  Serial.begin(115200);
+
   // Initialize the FastLED library
   FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
   delay(500);
   // Flash the lights RED twice
   flashLEDs(CRGB::Red, 2, 250);
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(
+    Task1code, /* Task function. */
+    "Task1",   /* name of task. */
+    10000,     /* Stack size of task */
+    NULL,      /* parameter of the task */
+    1,         /* priority of the task */
+    &Task1,    /* Task handle to keep track of created task */
+    0);        /* pin task to core 0 */
+  delay(500);
+
+  //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+  xTaskCreatePinnedToCore(
+    Task2code, /* Task function. */
+    "Task2",   /* name of task. */
+    10000,     /* Stack size of task */
+    NULL,      /* parameter of the task */
+    1,         /* priority of the task */
+    &Task2,    /* Task handle to keep track of created task */
+    1);        /* pin task to core 1 */
+  delay(500);
 }
 
-void loop() {
+void loop() {}
+
+void Task1code(void* pvParameters) {
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
   //generate a random integer between 1 and 10
   if ((int)random(11) > 8) {
     // if it's a 9 or a 10, do that flicker thing
@@ -34,6 +74,18 @@ void loop() {
   } else {
     // Otherwise switch to the new color
     fadeColor(colors[(int)random(1, numColors + 1)]);
+  }
+}
+
+void Task2code(void* pvParameters) {
+  Serial.print("Task2 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for (;;) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(700);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(700);
   }
 }
 
@@ -57,12 +109,12 @@ void flicker() {
   //flash the lights in white flashCount times
   //with a random duration and random delay between each flash
   for (int i = 0; i < flashCount; i++) {
-    // Set all pixels to white and turn them on    
+    // Set all pixels to white and turn them on
     fill_solid(leds, NUM_LEDS, CRGB::White);
     FastLED.show();
     // Delay for a random period of time (in milliseconds)
     delay((int)random(50, 150));
-    //clear the lights (set the color to none)    
+    //clear the lights (set the color to none)
     fill_solid(leds, NUM_LEDS, CRGB::Black);
     FastLED.show();
     // Delay for a random period of time (in milliseconds)
@@ -72,7 +124,7 @@ void flicker() {
 
 // Fill the NeoPixel array with a specific color
 void fadeColor(CRGB c) {
-  for (int i = 0; i <= 25; i++) {
+  for (int i = 0; i < 25; i++) {
     leds[i] = c;
     FastLED.show();
     delay(10);
