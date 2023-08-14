b485949badd6a174252ff74208b02c59f9e412b6
John M. Wargo <johnwargo@users.noreply.github.com>
2023-08-14 13:38:43 -0400

    got server working

--- glowing-pumpkin-xiao-bff-server.ino
+++ glowing-pumpkin-xiao-bff-server.ino
@@ -121,50 +121,20 @@ void Task2code(void* pvParameters) {
 
   for (;;) {
     WiFiClient client = server.available();  // Listen for incoming clients
-
-    if (client) {  // If a new client connects,
+    if (client) {                            // If a new client connects,
       Serial.println("Client connection");
-      String currentLine = "";                                                   // make a String to hold incoming data from the client
-      while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
-        currentTime = millis();
-        if (client.available()) {  // if there's bytes to read from the client,
-          char c = client.read();  // read a byte, then
-          Serial.write(c);         // print it out the serial monitor
-          header += c;
-          if (c == '\n') {  // if the byte is a newline character
-            // if the current line is blank, you got two newline characters in a row.
-            // that's the end of the client HTTP request, so send a response:
-            if (currentLine.length() == 0) {
-              // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-              // and a content-type so the client knows what's coming, then a blank line:
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-type:text/html");
-              client.println("Connection: close");
-              client.println();
-
-              // Display the HTML web page
-              client.println("<!DOCTYPE html><html>");
-              client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>");
-              client.println("<body><h1>ESP32 Web Server</h1>");
-              client.println("</body></html>");
-              // The HTTP response ends with another blank line
-              client.println();
-              // Break out of the while loop
-              break;
-            } else {  // if you got a newline, then clear currentLine
-              currentLine = "";
-            }
-          } else if (c != '\r') {  // if you got anything else but a carriage return character,
-            currentLine += c;      // add it to the end of the currentLine
-          }
-        }
-      }
-      // Clear the header variable
-      header = "";
-      // Close the connection
+      client.println("HTTP/1.1 200 OK");
+      client.println("Content-type:text/html");
+      client.println("Connection: close");
+      client.println();
+      client.println("<!DOCTYPE html><html>");
+      client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>");
+      client.println("<body><h1>ESP32 Web Server</h1>");
+      client.println("</body></html>");
+      client.println();
       client.stop();
       Serial.println("Client disconnected.");
-      Serial.println("");
+      Serial.println();
     }
   }
 }
